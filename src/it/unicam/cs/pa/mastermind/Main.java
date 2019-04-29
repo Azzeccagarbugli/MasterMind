7b047c6714f44203cd8521e9e9bc69b7dddd1163
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2019-04-29 14:29:03 +0200

    [UPDATE] Test JavaDoc

--- src/it/unicam/cs/pa/mastermind/Main.java
+++ src/it/unicam/cs/pa/mastermind/Main.java
@@ -2,6 +2,10 @@ package it.unicam.cs.pa.mastermind;
 
 public class Main {
 
+	/**
+	 * JavaDoc di prova per la classe <code> Main </code>
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Thanos culone");
 
