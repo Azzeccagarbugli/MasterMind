3fe8288403bbbd34e85cd87b4e0c87ebf4d7f523
azzeccagarbugli <f.coppola1998@gmail.com>
2019-05-28 13:02:49 +0200

    [ADD] Aggiunto test simulazione game

---
+++ test/it/unicam/cs/pa/mastermind/test/SimulationGame.java
@@ -0,0 +1,69 @@
+package it.unicam.cs.pa.mastermind.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import static java.time.Duration.ofMillis;
+
+import org.junit.jupiter.api.Test;
+
+import it.unicam.cs.pa.mastermind.gamecore.Board;
+import it.unicam.cs.pa.mastermind.gamecore.BoardController;
+import it.unicam.cs.pa.mastermind.gamecore.ColorPegs;
+
+/**
+ * Il seguente test simula il corretto funzionamento di una singola partita.
+ * 
+ * @author Francesco Pio Stelluti, Francesco Coppola
+ *
+ */
+class SimulationGame {
+
+	private int sequenceLenght = 4;
+	private int maxAttempts = 9;
+
+	private Board board;
+	private BoardController boardController;
+
+	@Test
+	void testSimulationGame() {
+		assertTimeout(ofMillis(5000), () -> {
+			board = new Board(sequenceLenght, maxAttempts);
+			boardController = new BoardController(board);
+			assertEquals(maxAttempts - board.leftAttempts(), boardController.numberOfAttemptsInserted());
+			boardController
+					.insertCodeToGuess(Arrays.asList(ColorPegs.RED, ColorPegs.GREEN, ColorPegs.BLUE, ColorPegs.CYAN));
+			boardController
+					.insertNewAttempt(Arrays.asList(ColorPegs.BLACK, ColorPegs.BLACK, ColorPegs.WHITE, ColorPegs.BLUE));
+			assertTrue(boardController.hasLeftAttempts());
+			assertFalse(boardController.hasBreakerGuessed());
+			assertEquals(maxAttempts - board.leftAttempts(), boardController.numberOfAttemptsInserted());
+			boardController
+					.insertNewAttempt(Arrays.asList(ColorPegs.WHITE, ColorPegs.RED, ColorPegs.WHITE, ColorPegs.YELLOW));
+			assertFalse(board.isEmpty());
+			assertEquals(Arrays.asList(ColorPegs.WHITE),
+					boardController.getClueFromAttempt(
+							Arrays.asList(ColorPegs.WHITE, ColorPegs.RED, ColorPegs.WHITE, ColorPegs.YELLOW),
+							Arrays.asList(ColorPegs.RED, ColorPegs.GREEN, ColorPegs.BLUE, ColorPegs.BLUE)));
+			boardController
+					.insertNewAttempt(Arrays.asList(ColorPegs.WHITE, ColorPegs.RED, ColorPegs.CYAN, ColorPegs.WHITE));
+			assertEquals(Arrays.asList(ColorPegs.WHITE, ColorPegs.WHITE),
+					boardController.getClueFromAttempt(
+							Arrays.asList(ColorPegs.WHITE, ColorPegs.RED, ColorPegs.CYAN, ColorPegs.WHITE),
+							Arrays.asList(ColorPegs.RED, ColorPegs.GREEN, ColorPegs.BLUE, ColorPegs.CYAN)));
+			assertEquals(maxAttempts - board.leftAttempts(), boardController.numberOfAttemptsInserted());
+			assertFalse(boardController.hasBreakerGuessed());
+			boardController
+					.insertNewAttempt(Arrays.asList(ColorPegs.RED, ColorPegs.GREEN, ColorPegs.BLUE, ColorPegs.CYAN));
+			assertEquals(Arrays.asList(ColorPegs.BLACK, ColorPegs.BLACK, ColorPegs.BLACK, ColorPegs.BLACK),
+					boardController.getClueFromAttempt(
+							Arrays.asList(ColorPegs.RED, ColorPegs.GREEN, ColorPegs.BLUE, ColorPegs.CYAN),
+							Arrays.asList(ColorPegs.RED, ColorPegs.GREEN, ColorPegs.BLUE, ColorPegs.CYAN)));
+			assertEquals(maxAttempts - board.leftAttempts(), boardController.numberOfAttemptsInserted());
+			assertFalse(board.isEmpty());
+			assertTrue(boardController.hasBreakerGuessed());
+		});
+	}
+
+}
