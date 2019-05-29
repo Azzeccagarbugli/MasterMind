43863235393080cd84e49a0aa03687500a6cd49e
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2019-05-29 11:42:30 +0200

    [UPDATE] Fixata gestione iniziale BufferedReader

--- src/it/unicam/cs/pa/mastermind/ui/ConsoleStartView.java
+++ src/it/unicam/cs/pa/mastermind/ui/ConsoleStartView.java
@@ -49,7 +49,7 @@ public class ConsoleStartView implements StartView {
 			+ "                                                       \r\n" + "";
 	private String mastermindCaptionStart = "Welcome player, play and have fun!";
 	private String mastermindCaptionEnd = "Thank you for taking part in this game, see you!";
-	private BufferedReader reader;
+
 	/**
 	 * Riferimento all'instanza Singleton.
 	 */
@@ -66,7 +66,6 @@ public class ConsoleStartView implements StartView {
 		lowTresholdAttempts = 1;
 		makers = new MakerFactoryRegistry();
 		breakers = new BreakerFactoryRegistry();
-		reader = new BufferedReader(new InputStreamReader(System.in));
 	}
 
 	/**
@@ -81,13 +80,9 @@ public class ConsoleStartView implements StartView {
 
 	@Override
 	public void start() {
-		try {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));) {
+
 			while (toContinue) {
-				/*
-				 * Applicazione del pattern del Singleton attarverso il get dell'instanza per
-				 * poi passargli il BuffereReader generato in precedenza. Ciò per garantire
-				 * all'interno del software una sola instanza di una specifica classe.
-				 */
 				intView = ConsoleInteractionView.getIstance();
 				intView.init(reader);
 				System.out.format(ANSI_CYAN_BOLD + "%-1s " + ANSI_YELLOW + "%43s" + ANSI_RESET + "\n\n\n",
@@ -95,29 +90,29 @@ public class ConsoleStartView implements StartView {
 				if (!keepSettings) {
 					attempts = 9;
 					sequenceLength = 4;
-					setupNewPlayers();
+					setupNewPlayers(reader);
 
-					if (this.askNewSettings()) {
-						attempts = askNewAttempts(lowTresholdAttempts);
-						sequenceLength = askNewlength(lowTresholdLength, highTresholdLength);
+					if (this.askNewSettings(reader)) {
+						attempts = askNewAttempts(lowTresholdAttempts, reader);
+						sequenceLength = askNewlength(lowTresholdLength, highTresholdLength, reader);
 					}
 				}
 				System.out.println("\nNow starting the game");
 				currentGame = new SingleGame(this.sequenceLength, this.attempts, this.intView, currentBreaker,
 						currentMaker);
 				currentGame.start();
-				NewGameStats newStats = this.getNewSettings();
+				NewGameStats newStats = this.getNewSettings(reader);
 				this.toContinue = newStats.getContinue();
 				this.keepSettings = newStats.getKeepSettings();
 				this.clearScreen();
 			}
 		} catch (IOException e) {
-			System.out.print(e.getMessage());
+			e.printStackTrace();
 		}
 		this.ending();
 	}
 
-	private NewGameStats getNewSettings() {
+	private NewGameStats getNewSettings(BufferedReader reader) {
 		int intInput = 0;
 		try {
 			System.out.println("\nThe game has finished, what would you like to do now?");
@@ -126,7 +121,7 @@ public class ConsoleStartView implements StartView {
 						+ "• Start a new game with different settings [2]" + "\n" + "• Exit from the game [3]"
 						+ "\n> ");
 				try {
-					intInput = Integer.parseInt(this.reader.readLine());
+					intInput = Integer.parseInt(reader.readLine());
 				} catch (NumberFormatException e) {
 					System.out.println("Please insert a numeric value");
 				}
@@ -171,7 +166,7 @@ public class ConsoleStartView implements StartView {
 	 * @throws NumberFormatException
 	 * @throws IOException
 	 */
-	private void setupNewPlayers() throws NumberFormatException, IOException {
+	private void setupNewPlayers(BufferedReader reader) throws NumberFormatException, IOException {
 		MakerFactory mFactory = (MakerFactory) makers.getFactoryByName(getPlayerName(makers, false, reader));
 		BreakerFactory bFactory = (BreakerFactory) breakers.getFactoryByName(getPlayerName(breakers, true, reader));
 		currentMaker = mFactory.getMaker();
@@ -217,7 +212,7 @@ public class ConsoleStartView implements StartView {
 	 * @return un array di booleani contenente i settaggi per la prossima partita
 	 * @throws IOException se il valore inserito non è un valore numerico
 	 */
-	private boolean askNewSettings() throws IOException {
+	private boolean askNewSettings(BufferedReader reader) throws IOException {
 		String strInput = "";
 		while (!(strInput.toLowerCase().equals("y") ^ strInput.toLowerCase().equals("n"))) {
 			System.out.print(
@@ -237,7 +232,7 @@ public class ConsoleStartView implements StartView {
 	 * @return il nuovo numero di tentativi
 	 * @throws IOException se il valore inserito non è un valore numerico
 	 */
-	private int askNewAttempts(int lowTres) throws IOException {
+	private int askNewAttempts(int lowTres, BufferedReader reader) throws IOException {
 		int attempts = 0;
 		do {
 			try {
@@ -261,7 +256,7 @@ public class ConsoleStartView implements StartView {
 	 * @return la nuova lunghezza della sequenza da inserire
 	 * @throws IOException se il valore inserito non è un valore numerico
 	 */
-	private int askNewlength(int lowTres, int highTres) throws IOException {
+	private int askNewlength(int lowTres, int highTres, BufferedReader reader) throws IOException {
 		int length = 0;
 		do {
 			try {
